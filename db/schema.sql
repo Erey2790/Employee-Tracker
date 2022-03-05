cabe241f36937d3aaff08cf288cd2fa7a9fa5b43
ElmerPC <erey2790@gmail.com>
2022-03-05 02:55:02 -0600

    created my schema file

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL,
+
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department
+        FOREIGN KEY (department_id)
+        REFRENCES department(id)
+        ON DELETE SET NULL
+    
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INT,
+    CONSTRAINT fk_role
+        FOREIGN KEY (role_id)
+        REFRENCES role(id)
+        ON SET NULL
+);
